600b858eddc6113b89f28f4a9cda02f3b22698cd
Thein Lwin Htun <theinlwin@blue-stone.net>
2020-08-09 17:52:27 +0630

    [change tomcat version]

--- src/main/java/com/finder/innox/EcoshopApplication.java
+++ src/main/java/com/finder/innox/EcoshopApplication.java
@@ -1,13 +1,19 @@
 package com.finder.innox;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
-public class EcoshopApplication {
+@ComponentScan(basePackages = { "com.finder.innox" })
+public class EcoshopApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EcoshopApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcoshopApplication.class, args);
--- src/main/java/com/finder/innox/MvcConfiguration.java
+++ src/main/java/com/finder/innox/MvcConfiguration.java
@@ -1,15 +1,22 @@
 package com.finder.innox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 import com.finder.innox.utils.CommonConstant;
 
@@ -37,6 +44,19 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
+    }
+	
+	@Bean
+    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setContentNegotiationManager(manager);
+        resolver.setOrder(1);
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        // Define all possible view resolvers
+        List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+        resolvers.add(viewResolver);
+        resolver.setViewResolvers(resolvers);
+        return resolver;
     }
 
 	@Override
